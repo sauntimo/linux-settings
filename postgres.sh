26569880b49f0a74038a80f835dbba002e36d7ac
Tim Saunders <tim@sauntimo.org>
2020-03-18 07:06:20 +0000

    Add script for new sqitch migrations

--- postgres.sh
+++ postgres.sh
@@ -10,6 +10,11 @@ _pgq(){
     PGPASSWORD=$PGPASSWORD PGOPTIONS=--search_path=api psql -X -h psql.dev.travellocal.com -U postgres -d travellocal_dev -c "$@"
 }
 
+# run a query from the command line. Quiet, ie returns tuples only (ie no headings/row counts) 
+_pgqq(){
+    PGPASSWORD=$PGPASSWORD PGOPTIONS=--search_path=api psql -t -X -h psql.dev.travellocal.com -U postgres -d travellocal_dev -c "$@"
+}
+
 # dev db test query
 _pgu(){
     _pgq 'SELECT * FROM "user" LIMIT 3;'
@@ -28,4 +33,17 @@ _pgqt(){
 # dev db test query
 _pgtu(){
     _pgqt 'SELECT * FROM "user" LIMIT 3;'
+}
+
+# Creates a new sqitch migration
+_sm(){
+    if [ -z "$1" ]
+    then
+        echo "Exiting as migration name was provided."
+        return 0
+    fi
+
+    # get the highest available tag from the db. xargs trims whitespace...
+    tag=$(_pgqq "SELECT tag FROM sqitch.tags ORDER BY tag DESC LIMIT 1;" | xargs)
+    sqitch add --all -r "$tag" "$1"
 }
